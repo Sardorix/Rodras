58ed3421e0fafb498db0dc81feae05fdf70628d9
Sardorix <101948058+Sardorix@users.noreply.github.com>
2022-03-20 23:38:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+function Block(letter, rate, used, father, code) {
+this.letter - letter;
+this.rate = rate;
+this.used = used;
+this.father = father;
+this.code = code;
+function FindCode(block){
+if (tree[block.father].code 1=
+block.code = tree[block.father].code +
+else {
+if (block.letter
+block.code
+tree[minIndex]-letter){
+*O":
+else if (block.letter == tree[preminIndex].letter) {
+block.code -
+else f
+FindCode(tree[block.father]);
+block.code
+= tree[block. father].code +
+"O"A
+}
+}
+}
